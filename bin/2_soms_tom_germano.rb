79ebf40d7a2f0e3e3f8f22ad51833cd62ee486d8
Bernardo <bersimoes@gmail.com>
2014-05-27 13:06:23 +0100

    added 1500 epoch to the som trainning

--- bin/2_soms_tom_germano.rb
+++ bin/2_soms_tom_germano.rb
@@ -6,7 +6,7 @@ require_relative '../lib/som.rb'
 #  A) Initialize the SOM with 100 random neurons,
 #     add 1000 random input patterns
 som = SOM::SOM.new output_space_size: 15,
-                   epochs: 5
+                   epochs: 1500
 ## Randomly fill the output space
 (225).times{ som.output_space.add(SOM::Neuron.new(3){ rand 0..255 }) }
 
