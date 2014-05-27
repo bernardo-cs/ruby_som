f417d9668a69af2d8297507ddf0fbf60c1afaa45
Bernardo <bersimoes@gmail.com>
2014-05-27 17:04:03 +0100

    change bin2 to 1500 epochs

--- bin/2_soms_tom_germano.rb
+++ bin/2_soms_tom_germano.rb
@@ -6,7 +6,7 @@ require_relative '../lib/som.rb'
 #  A) Initialize the SOM with 100 random neurons,
 #     add 1000 random input patterns
 som = SOM::SOM.new output_space_size: 15,
-                   epochs: 5
+                   epochs: 1500
 ## Randomly fill the output space
 (225).times{ som.output_space.add(SOM::Neuron.new(3){ rand 0..255 }) }
 
@@ -15,4 +15,4 @@ som.input_patterns = 1500.times.inject([]){ |arr| arr << Array.new(3){ rand(0..2
 
 som.exec_and_print_steps!('test_folder_3')
 
-`say Finished training everything, Sir`
+#`say Finished training everything, Sir`
