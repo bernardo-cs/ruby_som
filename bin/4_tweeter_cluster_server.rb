d28c009bf0fd7b3b4ca9e4cdc7f8fc5d4fa96cc2
Bernardo Simoes <bersimoes@gmail.com>
2014-07-11 18:50:43 +0100

    small edits on server executable

--- bin/4_tweeter_cluster_server.rb
+++ bin/4_tweeter_cluster_server.rb
@@ -15,17 +15,19 @@ puts inspect_dataset_stats
 @bin_matrix = BinMatrix.new( @csv_matrix_file.path, tweets_in_selected_words.uniq, 0)
 
 som = SOM::SOM.new output_space_size: 5,
-                   epochs: 600
+                   epochs: 1
 
  ## Randomly fill the output space
 (25).times{ som.output_space.add(SOM::Neuron.new(@bin_matrix.bin_matrix.first.size){ rand 0..1 }) }
 
 som.input_patterns = @bin_matrix
 som.exec!
-puts "Generating Report...."
+som.create_umatrix
 
+puts "Generating Report...."
 som.report do |f, n, t|
   f.puts( convert_stemmed_text_to_text( t ))
+  puts "Data written to: #{f.path}"
 end
 
 binding.pry
