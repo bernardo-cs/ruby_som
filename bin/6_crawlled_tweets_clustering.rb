6986aa9431ab6003e3ef2fac94b8c0166d14eb7a
Bernardo <bersimoes@gmail.com>
2014-07-29 18:35:31 +0100

    added new clustering bin for crawlled tweets

---
+++ bin/6_crawlled_tweets_clustering.rb
@@ -0,0 +1,23 @@
+require_relative "../../mini_twitter/lib/social_network.rb"
+require_relative "../../dataset_mapper/lib/dataset_mapper"
+require_relative "../../data_parser/lib/data_parser/bin_matrix.rb"
+require_relative "../../som/lib/som"
+
+social_network = MiniTwitter::SocialNetwork.unmarshal_latest!()
+puts 'Social Network in Usage Info', social_network.to_s
+
+@csv_matrix_file = Tempfile.new('csv_matrix')
+@bin_matrix = BinMatrix.new( @csv_matrix_file.path, social_network.all_tweets_trimmed_text, 0)
+binding.pry
+
+som = SOM::SOM.new output_space_size: 5
+
+ ## Randomly fill the output space
+(25).times{ som.output_space.add(SOM::Neuron.new(@bin_matrix.bin_matrix.first.size){ rand 0..1 }) }
+
+som.input_patterns = @bin_matrix
+som.exec!
+som.create_umatrix
+
+puts "Generating Report...."
+som.report( report_neuron_text: true )
