26958d568508aaa45b0fa9a043f6d7b82f4ade06
Bernardo <bersimoes@gmail.com>
2014-08-05 18:07:28 +0100

    fixed a stupid comma bug that I was sure it was fixed earlier...

--- bin/4_tweeter_cluster_server.rb
+++ bin/4_tweeter_cluster_server.rb
@@ -14,7 +14,7 @@ puts inspect_dataset_stats
 @csv_matrix_file = Tempfile.new('csv_matrix')
 @bin_matrix = BinMatrix.new( @csv_matrix_file.path, tweets_in_selected_words.uniq, 0)
 
-som = SOM::SOM.new output_space_size: 5,
+som = SOM::SOM.new output_space_size: 5
 
  ## Randomly fill the output space
 (25).times{ som.output_space.add(SOM::Neuron.new(@bin_matrix.bin_matrix.first.size){ rand 0..1 }) }
